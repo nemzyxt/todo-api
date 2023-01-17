75c7d8f62de88e8604465a9d8883e1ffcf8f639a
Nemuel Wainaina <bR0S3@protonmail.com>
2023-01-17 15:32:01 +0300

    Set up the router

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+)
+
 func main() {
-	
+	router := mux.NewRouter()
+
+	fmt.Println("[*] Server listening on port ...")
+	http.ListenAndServe(":8080", handlers.CORS()(router))
 }
